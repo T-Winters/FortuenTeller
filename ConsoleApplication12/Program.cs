4b93569d803c03ae88d2ff74db7d808f75a665ff
Tony Winters <twint022@gmail.com>
2016-07-08 18:40:33 -0400

    inital commit

---
+++ ConsoleApplication12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Part 1
+            //Ask the user for their first and last name, age, birth month, favorite ROYGBIV color
+            //If the user does not know what ROYGBIV is, ask them to enter “Help” 
+            //(print with the quotation marks) to get a list of the ROYGBIV colors
+
+            Console.WriteLine("What is your first name?");
+            Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            Console.ReadLine();
+            Console.WriteLine("What month were you born in?");
+            Console.ReadLine();
+            Console.WriteLine("What is your favorite ROYOGBIV color?\n If you're unsure enter \"Help.");
+            
+
+            Console.WriteLine("How many siblings do you have?");
+            Console.ReadLine();
+
+            Console.ReadKey();
+        }
+    }
+}
