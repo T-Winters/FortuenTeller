0cbdc67ea769b5c60ae5b440670bbdd140272bf0
Tony Winters <twint022@gmail.com>
2016-07-10 21:33:17 -0400

    3rd commit -touch ups

--- ConsoleApplication12/Program.cs
+++ ConsoleApplication12/Program.cs
@@ -10,179 +10,178 @@ namespace ConsoleApplication12
     {
         static void Main(string[] args)
         {
-            //Part 1
-            //Ask the user for their first and last name, age, birth month, favorite ROYGBIV color
-            //If the user does not know what ROYGBIV is, ask them to enter “Help” 
-            //(print with the quotation marks) to get a list of the ROYGBIV colors
-
-            //First Name
-            string firstName;
-            Console.WriteLine("What is your first name?");
-            firstName = (Console.ReadLine().ToLower());
-
-            //Last Name
-            string lastName;
-            Console.WriteLine("What is your last name?");
-            lastName = (Console.ReadLine().ToLower());
-
-            //Age
-            Console.WriteLine("How old are you?");
-            int age = int.Parse(Console.ReadLine());
-
-            //Birthmonth
-            string birthMonth;
-            Console.WriteLine("What month were you born in?");
-            birthMonth = (Console.ReadLine().ToLower());
-
-            //Favorite ROYOGBIV color and siblings
-
-            string userRoygbiv;
-
-            Console.WriteLine("What is your favorite ROYGBIV color?\n If you're unsure enter \"Help\" for a list of the ROYGBIV colors.");
-            userRoygbiv = (Console.ReadLine().ToLower());
-
-            //siblings
-            int siblings;
-            
-            if (userRoygbiv == "red" || userRoygbiv == "orange" || userRoygbiv == "yellow" || userRoygbiv == "green" || userRoygbiv == "blue" ||
-            userRoygbiv == "indigo" || userRoygbiv == "violet")
+            string playAgain = null;
+            do
             {
-                Console.WriteLine("How many siblings do you have?");
-                siblings = int.Parse(Console.ReadLine());
-            }
-            else if (userRoygbiv == "\"help\"")
-            {
-                Console.WriteLine("Choose from a ROYGBIV color below:");
-                Console.WriteLine("\nRed \nOrange \nYellow \nGreen \nBlue \nIndigo \nViolet");
-                userRoygbiv = (Console.ReadLine().ToLower());
-                Console.WriteLine("How many siblings do you have?");
-                siblings = int.Parse(Console.ReadLine());
-            }
-            else
-            {
+                //Part 1
+                //Ask the user for their first and last name, age, birth month, favorite ROYGBIV color
+                //If the user does not know what ROYGBIV is, ask them to enter “Help” 
+                //(print with the quotation marks) to get a list of the ROYGBIV colors
 
-            }
+                //First Name
+                Console.WriteLine("What is your first name?");
+                string firstName = (Console.ReadLine().ToLower());
 
 
-                /*if (userRoygbiv == "\"help\"")
-                    {
-                        Console.WriteLine("Choose from a ROYGBIV color below:");
-                        Console.WriteLine("\nRed \nOrange \nYellow \nGreen \nBlue \nIndigo \nViolet");                        
-                    }
+                //Last Name
+                Console.WriteLine("What is your last name?");
+                string lastName = (Console.ReadLine().ToLower());
+
+
+                //Age
+                Console.WriteLine("How old are you?");
+                int age = int.Parse(Console.ReadLine());
+
+
+                //Birthmonth
+
+                Console.WriteLine("What month were you born in?");
+                string birthMonth = (Console.ReadLine().ToLower());
+
+
+                //Favorite ROYOGBIV color and siblings
+
+                Console.WriteLine("What is your favorite ROYGBIV color?\n If you're unsure enter \"Help\" for a list of the ROYGBIV colors.");
+                string userRoygbiv = (Console.ReadLine().ToLower());
+
+                //siblings
+
+                int siblings = 0;
+                if (userRoygbiv == "red" || userRoygbiv == "orange" || userRoygbiv == "yellow" || userRoygbiv == "green" || userRoygbiv == "blue" ||
+                userRoygbiv == "indigo" || userRoygbiv == "violet")
+                {
+                    Console.WriteLine("How many siblings do you have?");
+                    siblings = int.Parse(Console.ReadLine());
                 }
+                else if (userRoygbiv == "\"help\"")
+                {
+                    Console.WriteLine("Choose from a ROYGBIV color below:");
+                    Console.WriteLine("\nRed \nOrange \nYellow \nGreen \nBlue \nIndigo \nViolet");
+                    userRoygbiv = (Console.ReadLine().ToLower());
+                    Console.WriteLine("How many siblings do you have?");
+                    siblings = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                }
+
+                //PART TWO
+                //If the user’s age is an odd number, then they will retire in ____ years, even number ____ years.
+                //If user has 0 siblings they they will live in ___ location, 1 sibling ___, 2 siblings, 3 siblings, 3> ___.
+                //Depending on which ROYGBIV color is the user’s favorite, they will have a specific mode of transportation.
+                //Retirement
+
+                int retirement = 0;
+                if (age % 2 == 1)
+                {
+                    retirement = 25;
+                }
+                else if (age % 2 == 0)
+                {
+                    retirement = 10;
+                }
+                else
+                {
+                }
+
+
+                //Where you'll live
+
+                string location;
+
+                if (siblings == 0)
+                {
+                    location = "Location A";
+                }
+                else if (siblings == 1)
+                {
+                    location = "Location B";
+                }
+                else if (siblings == 2)
+                {
+                    location = "Location C";
+                }
+                else if (siblings == 3)
+                {
+                    location = "Location D";
+                }
+                else
+                {
+                    location = "Location E";
+                }
+
+
+                //Mode of Transportation
+
+                string transportation = null;
+                if (userRoygbiv == "red")
+                {
+                    transportation = "a pair of roller blades";
+                }
+                else if (userRoygbiv == "orange")
+                {
+                    transportation = "an Oldsmobile Cutlass";
+                }
+                else if (userRoygbiv == "yellow")
+                {
+                    transportation = "a Yamaha YXZ1000R off-road buggy";
+                }
+                else if (userRoygbiv == "green")
+                {
+                    transportation = "a golf cart";
+                }
+                else if (userRoygbiv == "blue")
+                {
+                    transportation = "a bulldozer";
+                }
+                else if (userRoygbiv == "indigo")
+                {
+                    transportation = "an Imperia GP";
+                }
+                else if (userRoygbiv == "violet")
+                {
+                    transportation = "a 1960s Volkswagen van";
+                }
+                else
+                {
+                }
+
+
+                //Money in the bank
+
+                int moneyInTheBank = 0;
+                if (firstName.Contains(birthMonth[0]) || lastName.Contains(birthMonth[0]))
+                {
+                    moneyInTheBank = 100;
+                }
+                else if (firstName.Contains(birthMonth[1]) || lastName.Contains(birthMonth[1]))
+                {
+                    moneyInTheBank = 10000;
+                }
+                else if (firstName.Contains(birthMonth[2]) || lastName.Contains(birthMonth[2]))
+                {
+                    moneyInTheBank = 100000;
+                }
+                else if (!(firstName.Contains(birthMonth)) || !(lastName.Contains(birthMonth)))
+                {
+                    moneyInTheBank = 10000000;
+                }
+
+
+                //PART THREE
+
+                Console.WriteLine("Your Fortune:\n\n{0} {1} will retire in {2} years, with ${3} in the bank, a vacation home in {4} and {5}.",
+                    firstName, lastName, retirement, moneyInTheBank, location, transportation);
+
+                Console.WriteLine("\nWould you like to play again?");
+                playAgain = Console.ReadLine().ToLower();
+                Console.Clear(); ;
             }
 
-            Console.WriteLine("How many siblings do you have?");
-            siblings = int.Parse(Console.ReadLine());*/
+            while (playAgain == "yes");
 
             Console.ReadKey();
-            //PART TWO
-            //If the user’s age is an odd number, then they will retire in ____ years, even number ____ years.
-            //If user has 0 siblings they they will live in ___ location, 1 sibling ___, 2 siblings, 3 siblings, 3> ___.
-            //Depending on which ROYGBIV color is the user’s favorite, they will have a specific mode of transportation.
 
-            //Retirement
-
-            int retirement;
-
-            if (age % 2 == 1)
-            {
-                retirement = 25;
-            }
-            else if (age % 2 == 0)
-            {
-               retirement = 10 ;
-            }
-            else
-            {
-            }
-            
-            //Where you'll live
-
-            if (siblings == 0)
-            {
-                Console.WriteLine("You will live in location A.");
-            }
-            else if (siblings == 1)
-            {
-                Console.WriteLine("You will live in location B.");
-            }
-            else if (siblings == 2)
-            {
-                Console.WriteLine("You will live in location C.");
-            }
-            else if (siblings == 3)
-            {
-                Console.WriteLine("You will live in location D.");
-            }
-            else
-            {
-                Console.WriteLine("You will live in location E.");
-            }
-        
-    
-            //Mode of Transportation
-
-            if (userRoygbiv == "red")
-            {
-                Console.WriteLine("a pair roller blades.");
-            }
-            else if (userRoygbiv == "orange")
-            {
-                Console.WriteLine("an Oldsmovile Cutlass.");
-            }
-            else if (userRoygbiv == "yellow")
-            {
-                Console.WriteLine("a Yamaha YXZ1000R off-road buggy.");
-            }
-            else if (userRoygbiv == "green")
-            {
-                Console.WriteLine("a golf cart.");
-            }
-            else if (userRoygbiv == "blue")
-            {
-                Console.WriteLine("a bulldozer");
-            }
-            else if (userRoygbiv == "indigo")
-            {
-                Console.WriteLine("an Imperia GP.");
-            }
-            else if (userRoygbiv == "violet")
-            {
-                Console.WriteLine("a 1960's Volkswagen van.");
-            }
-            else
-            {
-
-            }
-            //Money in the bank
-
-            if (firstName.Contains(birthMonth[0])  || lastName.Contains(birthMonth[0]))
-            {
-                Console.WriteLine("You will have $100 in your bank account.");
-            }
-            else if (firstName.Contains(birthMonth[1]) || lastName.Contains(birthMonth[1]))
-            {
-                Console.WriteLine("You will have $10,000 in your bank account.");
-            }
-            else if (firstName.Contains(birthMonth[2]) || lastName.Contains(birthMonth[2]))
-            {
-                Console.WriteLine("You will have $100,000 in your bank account.");
-            }
-            /* else if (!(firstName.Contains(birthMonth[])) || !(lastName.Contains(birthMonth[])))
-             {
-                 Console.WriteLine("You will have $10,000,000 in your bank account.");   
-             }*/
-
-            //PART THREE
-
-            Console.WriteLine("Your Fortune:");
-            
-
-            
-            [First Name][Last Name] will retire in [# of Years] with [Amount of Money] in the bank, a vacation home in [Location] and a [Mode of Transportation].
-            Console.ReadKey();
-        
         }
     }
 }
